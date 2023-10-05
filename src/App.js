86c917918d8043fe86e705376089b64d9a337241
Abhinaba <abhi.roychoudhury.8@gmail.com>
2023-10-05 10:53:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import AddBirds from './Component/AddBirds';
+import PhoneBook from './Component/PhoneBook';
+import TaskManager from './Component/TaskManager';
+import Timer from './Component/Timer';
+import Covid from './Component/Covid';
+import Covid2 from './Component/Covid2';
+import Sort from './Component/Sort';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Component/Home'
+import About from './Component/About'
+import NavBar from './Component/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
